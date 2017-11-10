e3b1e762134b3026697f74bd8e197c195dbdf2f8
OVI <OVI@DESKTOP-DF5TG5J>
2017-11-11 02:05:29 +0600

    1st step

---
+++ WebAuthenticationWithRefreshToken/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebAuthenticationWithRefreshToken.Startup))]
+
+namespace WebAuthenticationWithRefreshToken
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
